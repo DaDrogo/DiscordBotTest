5b78e8c080b952c81db76327a70564ff0be767ff
DaDrogo <38428733+DaDrogo@users.noreply.github.com>
2019-09-03 04:15:13 +0200

    git.commit

--- Test.py
+++ Test.py
@@ -1,13 +1,18 @@
 import discord
 from discord.ext import commands
 
-TOKEN='NjE3NzU3MzM2NjcyNTM0NTk4.XWvzMQ.Hx_wd4tSadFPzMzHT3PRpxqQp1A'
+TOKEN='NjE3NzU3MzM2NjcyNTM0NTk4.XW3Ldw.qUmwIfFT07JflovTnzIjHAvuGZA'
 
 client = commands.Bot(command_prefix='test')
 
 @client.event
 async def on_ready():
     print('bot is rdy.')
-
+    
+@client.event
+async def on_message(message):
+    if message.content.startswith('!test'):
+        await message.channel.send('hello world!')
+    
 client.run(TOKEN)
     
