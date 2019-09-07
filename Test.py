27d65576ca8c43684dd96fc2743f9e470f3934fe
DaDrogo <38428733+DaDrogo@users.noreply.github.com>
2019-09-07 05:53:01 +0200

    learned loops and random

--- Test.py
+++ Test.py
@@ -1,9 +1,11 @@
 import discord
+import random
 from discord.ext import commands
 
-TOKEN='NjE3NzU3MzM2NjcyNTM0NTk4.XW3Ldw.qUmwIfFT07JflovTnzIjHAvuGZA'
-
+TOKEN=''
 client = commands.Bot(command_prefix='test')
+rnd = random.randint(1,1000)
+
 
 @client.event
 async def on_ready():
@@ -11,8 +13,20 @@ async def on_ready():
     
 @client.event
 async def on_message(message):
-    if message.content.startswith('!test'):
+    if message.content.startswith('!test'):        
         await message.channel.send('hello world!')
+        await message.channel.send('hello world!')
+    if message.content.startswith('!rnd'):
+        await message.channel.send(rnd)
+    if message.content.startswith('!magic'):
+        rndAnswe = random.randint(1,2)
+        if rndAnswe == 1:
+            answer = 'yes'
+        else:
+            answer = 'no' 
+        await message.channel.send(rndAnswe)
+        await message.channel.send(answer)
+        
     
 client.run(TOKEN)
     
